f5f7618c70cd73437d31ad9380cec840d7c290fd
Juan Cetina <jcetinapanzano@gmail.com>
2023-03-21 10:47:20 +0100

    test: update setup file and coverage files

--- tests/setupFile.js
+++ tests/setupFile.js
@@ -5,22 +5,9 @@ const localStorageMock = (function () {
         getItem(key) {
             return store[key];
         },
-
         setItem(key, value) {
             store[key] = value;
-        },
-
-        clear() {
-            store = {};
-        },
-
-        removeItem(key) {
-            delete store[key];
-        },
-
-        getAll() {
-            return store;
-        },
+        }
     };
 })();
 
